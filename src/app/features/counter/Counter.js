4658a39bfb707e4fc6c3b06b52f96f51a66ef43f
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 15:45:54 +0200

    Use Redux State and Actions in React Components

--- src/app/features/counter/Counter.js
+++ src/app/features/counter/Counter.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
 
-const Counter = () => {
-    const count = useSelector((state => state.counter.count));
-    const dispatch = useDispatch()
+export function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
-    <section>
-      <p>{count}</p>
+    <div>
       <div>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
       </div>
-    </section>
+    </div>
   )
 }
-
 export default Counter
